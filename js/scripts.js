c3e8d02e6dafc80b3051af77e2b30f089afbf0ce
Michael Gacek <gacek.michael@outlook.com>
2018-04-19 20:02:19 +0200

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -4,12 +4,9 @@ var list = document.getElementById('list');
 var add = document.getElementById('addElem');
 
 add.addEventListener('click', function() {
-	var element = document.createElement('li');
-	element.innerHTML = 'item ';
+	var element = document.createElement('li'),
+        liElements = document.getElementsByTagName("li");
+
+	element.innerHTML = 'item ' + liElements.length;
 	list.appendChild(element);
 });
-
-function myFunction() {
-    var x = document.getElementsByTagName("li");
-    document.getElementById("addElem").innerHTML = x.length;
-}
